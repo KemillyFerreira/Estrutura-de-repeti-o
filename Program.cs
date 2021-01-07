d0cef42924d40a9bddae749723dc67167c44b023
Kemilly Ferreira <73208400+KemillyFerreira@users.noreply.github.com>
2021-01-07 01:28:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Estrutura_de_Repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Obs:De 0 a 10.");
+            Console.Write("Digite a nota: ");
+            int nota = int.Parse(Console.ReadLine());
+
+            while(nota > 10){
+                Console.WriteLine("A nota não é válida");
+                Console.Write("Digite o valor novamente: ");
+                nota = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Nota cadastrada!");
+        }
+    }
+}
